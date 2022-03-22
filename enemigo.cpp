2fe24c65d822dba706851c376fec3fa268e64da9
David <david.gonzalez12@udea.edu.co>
2022-03-21 20:31:23 -0500

    version estable, pero no funciona bien el mivimiento.

--- enemigo.cpp
+++ enemigo.cpp
@@ -24,8 +24,8 @@ void Enemigo::moveEnemy()
     if (tryMove(direction)) { //si es posible el movimiento
         int mX = ((int)x())/(size_sprites*sizeGame);
         int mY = ((int)y())/(size_sprites*sizeGame);
-        if ((int)x()%(size_sprites*sizeGame)==0 && (int)x()%(size_sprites*sizeGame)==0 && mX%2!=2 && mY%2!=0) { //si se esta en una intercepcion
-            if (even_aleatorio(0.3)) { //si cambia de direccion
+        if ((int)x()%(size_sprites*sizeGame)==0 && (int)y()%(size_sprites*sizeGame)==0 && mX%2!=0 && mY%2!=0) { //si se esta en una intercepcion
+            if (even_aleatorio(0.1)) { //si cambia de direccion
                 direction = changeDirection();
             }
             move(direction);
@@ -44,12 +44,12 @@ short Enemigo::changeDirection()
 {
     short buffer[4];
     short count = 0;
-    short posX = x();
-    short posY = y();
-    if (matrizGame[posY+1][posX] == 9) buffer[count] = 0, count++;
-    if (matrizGame[posY][posX-1] == 9) buffer[count] = 1, count++;
-    if (matrizGame[posY][posX+1] == 9) buffer[count] = 2, count++;
-    if (matrizGame[posY-1][posX] == 9) buffer[count] = 3, count++;
+    short mX = (int)x()/(size_sprites*sizeGame);
+    short mY = (int)y()/(size_sprites*sizeGame);
+    if (matrizGame[mY+1][mX] == 9) buffer[count] = 0, count++;
+    if (matrizGame[mY][mX-1] == 9) buffer[count] = 1, count++;
+    if (matrizGame[mY][mX+1] == 9) buffer[count] = 2, count++;
+    if (matrizGame[mY-1][mX] == 9) buffer[count] = 3, count++;
     if (count == 0) return 916;
     count = 0+rand()%(count-0);//genera numeros aleatorios en el rango de 0-count que corresponde a algun indice valido del buffer que contiene una direccion
     return buffer[count];
